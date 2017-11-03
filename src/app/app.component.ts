f71a90834959e900d969188910c95523e332c126
zhen qiu <youthrone@hotmail.com>
2017-11-03 16:22:47 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  mails: object[]=[
+    { email:"bill@gate.com", importance:true, subject: "New Windowns", content:'Windows XI will lanuch in year 2100.'},
+    { email:"ada@lovelace.com", importance:true, subject: "Programming", content:'Enchantress of Number.'},
+    { email:"john@carmac.com", importance:false, subject: "Updated Algo", content:'New algorithm for shadow volumes.'},
+    { email:"gabe@newel.com", importance:false, subject: "HL3!", content:'Just kidding....'}
+
+  ]
 }
