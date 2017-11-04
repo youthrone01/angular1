dab0fc3afec62df7a51b8cc2fa4cd958f781770e
zhen qiu <youthrone@hotmail.com>
2017-11-03 17:17:57 -0700

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   mails: object[]=[
     { email:"bill@gate.com", importance:true, subject: "New Windowns", content:'Windows XI will lanuch in year 2100.'},
@@ -14,4 +14,32 @@ export class AppComponent {
     { email:"gabe@newel.com", importance:false, subject: "HL3!", content:'Just kidding....'}
 
   ]
+
+  colors= ["Aqua","CadetBlue","Crimson","DarkGray","DarkMagenta","DarkSlateBlue","Gold","LightBlue","RoyalBlue",
+   "SandyBrown", "SteelBlue", "Tomato","Olive"]
+
+   shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  ngOnInit() {
+    this.shuffle(this.colors);
+  }
+  
+  
 }
